ea8d20f80e0e6fe35b10e3afe02a5512922b6ef2
carry0987 <carry0987@users.noreply.github.com>
2024-01-26 20:13:22 +0800

    Remove middle script

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import replace from '@rollup/plugin-replace';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import del from 'rollup-plugin-delete';
 import { createRequire } from 'module';
 const pkg = createRequire(import.meta.url)('./package.json');
 
@@ -21,7 +22,8 @@ const jsConfig = {
         replace({
             preventAssignment: true,
             __version__: pkg.version
-        })
+        }),
+        del({ hook: 'buildEnd', targets: ['dist/server.js', 'dist/interface', 'dist/type'] })
     ],
     external: ['axios', 'express', 'sharp', 'cryptr', 'dotenv', 'file-type']
 };
