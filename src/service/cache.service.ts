d76c33bb7988757d57da5baf6de465c493d562d4
carry0987 <carry0987@users.noreply.github.com>
2024-06-15 11:10:04 +0800

    Update cache.service.ts

--- src/service/cache.service.ts
+++ src/service/cache.service.ts
@@ -1,11 +1,17 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
+import { UtilsService } from '@/common/utils/utils.service';
+import { ImageFormat, ImageCache } from '@/common/type/types';
+import { ImageProcessingService } from './image-processing.service';
 import xxhash from 'xxhashjs';
-import { ImageCache } from '../common/type/types';
 
 @Injectable()
 export class CacheService {
-    constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+    constructor(
+        @Inject(CACHE_MANAGER) private cacheManager: Cache,
+        private utilsService: UtilsService,
+        private imageProcessingService: ImageProcessingService,
+    ) {}
 
     private generateCacheHash(url: string, options: ImageCache): string {
         const optionsString = JSON.stringify(options);
@@ -13,13 +19,58 @@ export class CacheService {
         return xxhash.h32(url + optionsString, 0xabcd).toString(16);
     }
 
-    async setCache(url: string, options: ImageCache): Promise<void> {
+    public async handleCache(
+        sourceURL: string,
+        format: ImageFormat,
+        width: number | undefined,
+        height: number | undefined,
+        suffix: string | undefined,
+        checkETag: boolean,
+        clientETag: string,
+    ): Promise<{ cachedPath: string | null; eTag: string | null }> {
+        const validCache = await this.validateCache(sourceURL, {
+            format,
+            width,
+            height,
+            suffix,
+        });
+        const savePathInfo = await this.imageProcessingService.getImageSavePath(
+            { sourcePath: sourceURL, format, originalFormat: format, suffix },
+        );
+        const imageExists = await this.utilsService.checkFileExists(
+            savePathInfo.path,
+        );
+
+        if (imageExists && validCache) {
+            if (checkETag) {
+                const cacheImage: Buffer =
+                    await this.utilsService.readFileAsync(savePathInfo.path);
+                const eTag = this.utilsService.generateETag(cacheImage, {
+                    width,
+                    height,
+                    suffix,
+                });
+                if (clientETag === eTag) {
+                    return { cachedPath: savePathInfo.path, eTag };
+                }
+                return { cachedPath: savePathInfo.path, eTag };
+            }
+            return { cachedPath: savePathInfo.path, eTag: null };
+        }
+
+        return { cachedPath: null, eTag: null };
+    }
+
+    public async setCache(url: string, options: ImageCache): Promise<void> {
         const hash = this.generateCacheHash(url, options);
         url += options.format ? `.${options.format}` : '';
         await this.cacheManager.set(url, hash);
     }
 
-    async validateCache(url: string, options: ImageCache): Promise<boolean> {
+    public async validateCache(
+        url: string,
+        options: ImageCache,
+    ): Promise<boolean> {
         const hash = this.generateCacheHash(url, options);
         url += options.format ? `.${options.format}` : '';
         const cacheValue = await this.cacheManager.get(url);
@@ -27,7 +78,7 @@ export class CacheService {
         return cacheValue === hash;
     }
 
-    async flushCache(): Promise<void> {
+    public async flushCache(): Promise<void> {
         await this.cacheManager.reset();
     }
 }
