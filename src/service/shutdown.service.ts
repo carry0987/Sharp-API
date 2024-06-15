e1b73a9431fc03837994a058c0f1fdd12797636f
carry0987 <carry0987@users.noreply.github.com>
2024-06-15 11:12:40 +0800

    Update shutdown.service.ts

--- src/service/shutdown.service.ts
+++ src/service/shutdown.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { Server } from 'http';
-import { UtilsService } from '../common/utils/utils.service';
+import { UtilsService } from '@/common/utils/utils.service';
 
 @Injectable()
 export class ShutdownService {
     constructor(private readonly utilsService: UtilsService) {}
 
-    gracefulShutdown(server: Server, signal: string) {
+    public gracefulShutdown(server: Server, signal: string) {
         console.info(`${signal} signal received.`);
         this.utilsService.handleResponse(null, 200, 'Closing http server.');
         server.close(() => {
